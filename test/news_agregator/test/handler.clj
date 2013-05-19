c0fe40b17f54d3a66c14241ba1b3836bdcc94c1a
Vitaliy Ladan <Vitaliy.Ladan@gmail.com>
2013-05-19 23:49:00 +0600

    Initial commit

---
+++ test/news_agregator/test/handler.clj
@@ -0,0 +1,14 @@
+(ns news_agregator.test.handler
+  (:use clojure.test
+        ring.mock.request  
+        news_agregator.handler))
+
+(deftest test-app
+  (testing "main route"
+    (let [response (app (request :get "/"))]
+      (is (= (:status response) 200))
+      (is (= (:body response) "Hello World"))))
+  
+  (testing "not-found route"
+    (let [response (app (request :get "/invalid"))]
+      (is (= (:status response) 404)))))
